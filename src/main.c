2445708c5e0cbd147914bf235b9dc667d82d53d4
Itachi <minhthuan1091996@gmail.com>
2022-04-22 00:33:43 +0700

    finish 3rd task

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 #include "FreeRTOS.h"
 #include "task.h"
 #include "semphr.h"
@@ -7,6 +8,10 @@
 #include "stm32f4xx.h"
 #include "utils.h"
 #include "lcd.h"
+
+#define LOG_DEBUG								1
+#define CONTROL_QUEUE_LEN				50
+#define THRESHOLD_DEFAULT				1500 //in mV
 
 typedef enum {
 	SENSOR_DATA=0,		//Send from sensor reading task
@@ -30,23 +35,18 @@ portTASK_FUNCTION_PROTO(vControl, pvParameters);
 
 // *** Declare a queue HERE
 static SemaphoreHandle_t xbinary_sem = NULL;
-
-
-
-static QueueHandle_t button_queue = NULL; 
-static SemaphoreHandle_t lcd_mutex = NULL;
-uint16_t data_adc1=0, data_adc2=0;
+static QueueHandle_t control_queue = NULL; 
 
 // ============================================================================
 int main(void) {
-
-	// Initialise all of the STM32F4DISCOVERY hardware (including the serial port)
+	#if LOG_DEBUG
 	vUSART2_Init();
+	#endif //LOG_DEBUG
 	vLED_Init();
 	vSW1_Init();
 	vADC1_Init();
 	vADC2_Init();
-
+	lcd_init();
 
 	xbinary_sem = xSemaphoreCreateBinary();
 	if(xbinary_sem == NULL)
@@ -55,36 +55,26 @@ int main(void) {
 		while(1)	//Hang in here
 		{
 
-		};
+		}
+	}
+	
+	control_queue = xQueueCreate(CONTROL_QUEUE_LEN, sizeof(data_msg_t)); 
+	if(control_queue == NULL)
+	{
+		printf("Cant create control queue \r\n ");
+		while(1)	//Hang in here
+		{
+
+		}
 	}
 
-	#if 0
-	
-	
-	// Button queue to handle button pressed events
-	button_queue = xQueueCreate(BUTT_QUEUE_LEN, sizeof(uint32_t)); 
-	assert_param(button_queue != NULL); 
-	lcd_mutex = xSemaphoreCreateMutex();
-	assert_param(lcd_mutex != NULL); 
-	#endif
-	// Tasks get started here.
-	// Arguments to xTaskCreate are:
-	// 1- The function to execute as a task (make sure it never exits!)
-	// 2- The task name (keep it short)
-	// 3- The stack size for the new task (configMINIMAL_STACK_SIZE == 130)
-	// 4- Parameter for the task (we won't be using this, so set it to NULL)
-	// 5- The task priority; higher numbers are higher priority, and the idle task has priority tskIDLE_PRIORITY
-	// 6- A pointer to an xTaskHandle variable where the TCB will be stored
 	xTaskCreate(vSensorSetting, "SENSOR_SETTING", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY+3, &hSensorSettingTask);
 	xTaskCreate(vSensorReading, "SENSOR_READING", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY+2, &hSensorReadingTask);
 	xTaskCreate(vControl, "CONTROL", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY+1, &hControlTask);
 	
-	
-
 	vTaskStartScheduler(); // This should never return.
 
-	// Will only get here if there was insufficient memory to create
-	// the idle task.
+	// Will only get here if there was insufficient memory to create the idle task.
 	while(1);  
 }
 
@@ -100,9 +90,8 @@ portTASK_FUNCTION(vSensorReading, pvParameters) {
 		sensor_data = sADC1_GetData();
 		sensor_data_msg.data_value = sensor_data;
 		//Sending to control task
-
-		printf("Sensor reading %d \r\n", sensor_data);
-		vTaskDelay(1000/portTICK_RATE_MS);
+		xQueueSend(control_queue, &sensor_data_msg, portMAX_DELAY);
+		vTaskDelay(5000/portTICK_RATE_MS);
 	}	
 }
 
@@ -119,41 +108,94 @@ portTASK_FUNCTION(vSensorSetting, pvParameters) {
 		xSemaphoreTake(xbinary_sem, portMAX_DELAY);
 		sensor_threashold = sADC2_GetData();
 		sensor_setting_msg.data_value = sensor_threashold;
-		printf("\r\n ====================User settings====================\r\n"); 
- 		printf("Sensor threshold setting %d \r\n", sensor_threashold);
-		printf("\r\n =======================================================\r\n	"); 
-		
-
-
 		//Send data to control task
-		#if 0
-		total_run_time = (xTaskGetTickCount()/portTICK_RATE_MS)/1000; //run time in secs
-		memset((void*)run_time_buffer, 0, 16);
-		sprintf(run_time_buffer, "%06d", total_run_time);
-		if(xSemaphoreTake(lcd_mutex, (1000/portTICK_PERIOD_MS)) == pdPASS)
-		{
-			lcd_move(sizeof("Run time:"), 0); 
-			lcd_print(run_time_buffer);
-			xSemaphoreGive(lcd_mutex);
-		}
-		#if DEBUG_LOG
-		printf("Task 2 Run time: %d (s)\r\n", total_run_time);
-		#endif /*End of DEBUG_LOG */
-		vTaskDelay(500/portTICK_RATE_MS);
-		#endif
+		xQueueSend(control_queue, &sensor_setting_msg, portMAX_DELAY);
 	}
 }
 
-// This task should run whenever a message is waiting in the queue.
+// Control task which receive data from sensor reading task and sensor setting task
 // ---------------------------------------------------------------------------- 
 portTASK_FUNCTION(vControl, pvParameters) {
-	while(1) {
-			//printf("Print task is running ..\r\n");
-			vTaskDelay(5000/portTICK_RATE_MS);
+	bool is_ledon=false;
+	data_msg_t data_recv = {0};
+	uint16_t sensor_reading=0;
+	uint16_t threshold_setting=THRESHOLD_DEFAULT;
+	char lcd_buffer[16]={0};
+	
+	#if LOG_DEBUG
+	printf("\r\n ====================User settings====================\r\n"); 
+	printf("Default threshold %d \r\n", threshold_setting);
+	printf("\r\n =======================================================\r\n	"); 	
+	#endif //LOG_DEBUG
+	lcd_move(0,0);
+	lcd_print("Threshold:");
+	
+	lcd_move(0,1);
+	lcd_print("Sensor data:");
 
-			
-		// *** Receive a message from the queue and print it HERE
+	while(1) 
+	{	
+		//Receive data
+		if(xQueueReceive(control_queue, &data_recv, portMAX_DELAY) == pdTRUE)
+		{
+			if(data_recv.type == SENSOR_SETTING)
+			{
+				threshold_setting = data_recv.data_value;
+				//Display to LCD
+				sprintf(lcd_buffer, "%04d", threshold_setting);
+				lcd_move(sizeof("Threshold:"), 0);
+				#if LOG_DEBUG
+				printf("\r\n ====================User settings====================\r\n"); 
+				printf("Sensor threshold setting %d \r\n", threshold_setting);
+				printf("\r\n =======================================================\r\n	"); 
+				#endif //LOG_DEBUG
+
+			}
+			else if(data_recv.type == SENSOR_DATA)
+			{
+				sensor_reading = data_recv.data_value;
+				//Display to LCD
+				sprintf(lcd_buffer, "%04d", sensor_reading);
+				lcd_move(sizeof("Sensor data:"), 1);
+				#if LOG_DEBUG
+				printf("Sensor reading %d \r\n", sensor_reading);
+				#endif //LOG_DEBUG
+			}
+			else
+			{
+
+			}
+		}
 		
+		if(is_ledon == false)
+		{
+			if (sensor_reading >= threshold_setting) 
+			{
+				//Turn on LED
+				GPIO_SetBits(GPIOD, GPIO_Pin_8);
+				is_ledon = true;
+				#if LOG_DEBUG
+				printf("\r\n =======================================================\r\n"); 
+				printf("LED ON \r\n");
+				printf("\r\n =======================================================\r\n	"); 
+	
+				#endif //LOG_DEBUG
+			}
+		}
+		else //led is on already
+		{
+			if (sensor_reading < threshold_setting) 
+			{
+				//Turn off LED
+				GPIO_ResetBits(GPIOD, GPIO_Pin_8);
+				is_ledon = false;
+				#if LOG_DEBUG
+				printf("\r\n =======================================================\r\n"); 
+				printf("LED OFF \r\n");
+				printf("\r\n =======================================================\r\n	"); 
+				#endif //LOG_DEBUG
+			}
+		}
 	}
 }
 // ============================================================================

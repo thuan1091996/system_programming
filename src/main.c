117e15c728e3cfd75984bf3c6da0852d9cd53135
Itachi <minhthuan1091996@gmail.com>
2022-04-22 01:00:14 +0700

    moving code and fixing typing

--- src/main.c
+++ src/main.c
@@ -9,6 +9,7 @@
 #include "utils.h"
 #include "lcd.h"
 
+
 #define LOG_DEBUG								1
 #define CONTROL_QUEUE_LEN				50
 #define THRESHOLD_DEFAULT				1500 //in mV
@@ -24,6 +25,13 @@ typedef struct data_msg
 	uint16_t	data_value;
 }data_msg_t;
 
+
+void vLED_Init(void);
+void vSW1_Init(void);
+void vADC1_Init(void);
+void vADC2_Init(void);
+uint16_t sADC1_GetData(void);
+uint16_t sADC2_GetData(void); 
 
 xTaskHandle hSensorReadingTask;
 xTaskHandle hSensorSettingTask;
@@ -125,7 +133,7 @@ portTASK_FUNCTION(vControl, pvParameters) {
 	#if LOG_DEBUG
 	printf("\r\n ====================User settings====================\r\n"); 
 	printf("Default threshold %d \r\n", threshold_setting);
-	printf("\r\n =======================================================\r\n	"); 	
+	printf("\r\n =======================================================\r\n"); 	
 	#endif //LOG_DEBUG
 	lcd_move(0,0);
 	lcd_print("Threshold:");
@@ -177,7 +185,7 @@ portTASK_FUNCTION(vControl, pvParameters) {
 				#if LOG_DEBUG
 				printf("\r\n =======================================================\r\n"); 
 				printf("LED ON \r\n");
-				printf("\r\n =======================================================\r\n	"); 
+				printf("\r\n =======================================================\r\n"); 
 	
 				#endif //LOG_DEBUG
 			}
@@ -192,7 +200,7 @@ portTASK_FUNCTION(vControl, pvParameters) {
 				#if LOG_DEBUG
 				printf("\r\n =======================================================\r\n"); 
 				printf("LED OFF \r\n");
-				printf("\r\n =======================================================\r\n	"); 
+				printf("\r\n =======================================================\r\n"); 
 				#endif //LOG_DEBUG
 			}
 		}
@@ -211,6 +219,173 @@ void EXTI9_5_IRQHandler(void)
 		portYIELD_FROM_ISR( xHigherPriorityTaskWoken );
 		EXTI_ClearITPendingBit(EXTI_Line8);
   }
+}
+
+
+// ============================================================================
+void vLED_Init(void)
+{
+  // Init LED pin (PD8)
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+  GPIO_InitTypeDef GPIO_InitStructure;
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+  GPIO_Init(GPIOD, &GPIO_InitStructure);
+
+  //Turn off LED
+  GPIO_ResetBits(GPIOD, GPIO_Pin_8);
+
+}
+
+// ============================================================================
+void vSW1_Init(void)
+{
+  // Enable GPIOE clock
+  EXTI_InitTypeDef EXTI_InitStructure;
+  GPIO_InitTypeDef GPIO_InitStructure;
+  NVIC_InitTypeDef NVIC_InitStructure;
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOE, ENABLE);
+	
+  // Enable SYSCFG clock
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_SYSCFG, ENABLE);
+	
+  // PE8 input
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+  GPIO_Init(GPIOE, &GPIO_InitStructure);
+
+  // Configure EXTI Line8
+  SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOE, EXTI_PinSource8);
+
+  EXTI_InitStructure.EXTI_Line = EXTI_Line8;
+  EXTI_InitStructure.EXTI_Mode = EXTI_Mode_Interrupt;
+  EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Rising;
+  EXTI_InitStructure.EXTI_LineCmd = ENABLE;
+  EXTI_Init(&EXTI_InitStructure);
+	
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_4);
+
+  // Enable and set EXTI Line8 Interrupt to the lowest priority
+  NVIC_InitStructure.NVIC_IRQChannel = EXTI9_5_IRQn;
+  NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 10;
+  NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+  NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+  NVIC_Init(&NVIC_InitStructure);
+}
+
+// ============================================================================
+/* ADC1 Initialization to reading sensor value (RV2) which connected to RV3 - ADC1-IN12
+ */
+void vADC1_Init(void)
+{
+  //Enable clocks for ADC and ADC pin
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE);
+    
+  // Init GPIO for ADC1 Channel 12 (PC2) pin as analog input
+  GPIO_InitTypeDef GPIO_InitStructure;
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AN;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL ;
+  GPIO_Init(GPIOC, &GPIO_InitStructure);
+
+  // ADC Init
+  ADC_InitTypeDef ADC_InitStructure;
+  ADC_CommonInitTypeDef ADC_CommonInitStructure;
+
+  // ADC Common Init
+  ADC_CommonInitStructure.ADC_Mode = ADC_Mode_Independent;
+  ADC_CommonInitStructure.ADC_Prescaler = ADC_Prescaler_Div2;
+  ADC_CommonInitStructure.ADC_DMAAccessMode = ADC_DMAAccessMode_Disabled;
+  ADC_CommonInitStructure.ADC_TwoSamplingDelay = ADC_TwoSamplingDelay_20Cycles;
+  ADC_CommonInit(&ADC_CommonInitStructure);
+
+  // ADC Init
+  ADC_InitStructure.ADC_Resolution = ADC_Resolution_12b;
+  ADC_InitStructure.ADC_ScanConvMode = DISABLE;
+  ADC_InitStructure.ADC_ContinuousConvMode = ENABLE;
+  ADC_InitStructure.ADC_ExternalTrigConvEdge = ADC_ExternalTrigConvEdge_None;
+  ADC_InitStructure.ADC_ExternalTrigConv = ADC_ExternalTrigConvEdge_None;
+  ADC_InitStructure.ADC_DataAlign = ADC_DataAlign_Right;
+  ADC_InitStructure.ADC_NbrOfConversion = 1;
+  ADC_Init(ADC1, &ADC_InitStructure);
+
+  // ADC Channel Init
+  ADC_RegularChannelConfig(ADC1, ADC_Channel_12, 1, ADC_SampleTime_480Cycles);
+
+  // ADC Enable 
+  ADC_Cmd(ADC1, ENABLE);
+	
+	// Start the conversion
+	ADC_SoftwareStartConv(ADC1);
+}
+
+// ============================================================================
+uint16_t sADC1_GetData(void) //s for short
+{
+  	uint16_t raw_adc = ADC_GetConversionValue(ADC1);
+		double cal_temp = raw_adc * 3000.0 / 4095;	// casting to double for accurately calculation
+		return (uint16_t)cal_temp;
+}
+
+// ============================================================================
+/* ADC2 Initialization to reading user setting threshold which connected to RV2 - ADC2-IN11
+ */
+void vADC2_Init(void)
+{
+  //Enable clocks for ADC and ADC pin
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC2, ENABLE);
+    
+  // Init GPIO for ADC2 Channel 11 (PC1) pin as analog input
+  GPIO_InitTypeDef GPIO_InitStructure;
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AN;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+  GPIO_Init(GPIOC, &GPIO_InitStructure);
+
+  // ADC Init
+  ADC_InitTypeDef ADC_InitStructure;
+  ADC_CommonInitTypeDef ADC_CommonInitStructure;
+
+  // ADC Common Init
+  ADC_CommonInitStructure.ADC_Mode = ADC_Mode_Independent;
+  ADC_CommonInitStructure.ADC_Prescaler = ADC_Prescaler_Div2;
+  ADC_CommonInitStructure.ADC_DMAAccessMode = ADC_DMAAccessMode_Disabled;
+  ADC_CommonInitStructure.ADC_TwoSamplingDelay = ADC_TwoSamplingDelay_20Cycles;
+  ADC_CommonInit(&ADC_CommonInitStructure);
+
+  // ADC Init
+  ADC_InitStructure.ADC_Resolution = ADC_Resolution_12b;
+  ADC_InitStructure.ADC_ScanConvMode = DISABLE;
+  ADC_InitStructure.ADC_ContinuousConvMode = ENABLE;
+  ADC_InitStructure.ADC_ExternalTrigConvEdge = ADC_ExternalTrigConvEdge_None;
+  ADC_InitStructure.ADC_ExternalTrigConv = ADC_ExternalTrigConvEdge_None;
+  ADC_InitStructure.ADC_DataAlign = ADC_DataAlign_Right;
+  ADC_InitStructure.ADC_NbrOfConversion = 1;
+  ADC_Init(ADC2, &ADC_InitStructure);
+
+  // ADC Channel Init
+  ADC_RegularChannelConfig(ADC2, ADC_Channel_11, 1, ADC_SampleTime_480Cycles);
+
+  // ADC Enable 
+  ADC_Cmd(ADC2, ENABLE);
+	
+	// Start the conversion
+	ADC_SoftwareStartConv(ADC2);
+}
+
+// ============================================================================
+uint16_t sADC2_GetData(void) //s for short
+{
+  	uint16_t raw_adc = ADC_GetConversionValue(ADC2);
+		double cal_temp = raw_adc * 3000.0 / 4095;	// convert to mV casting to double for accurately calculation
+		return (uint16_t)cal_temp;
 }
 
 // ============================================================================
